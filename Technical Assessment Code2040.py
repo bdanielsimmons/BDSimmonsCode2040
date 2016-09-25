46dd15878d71c45a76477394d359e8f1abe42628
Daniel Simmons <bdanielsimmons@gmail.com>
2016-09-24 20:45:55 -0400

    Update Technical Assessment Code2040.py

--- Technical Assessment Code2040.py
+++ Technical Assessment Code2040.py
@@ -1,37 +1,43 @@
-import json, requests, datetime
+import json, requests, datetime #Used to encode dictionary, post/pull from APIs, and gather datetime information
 
-TOKEN = '6ecdff3c2d49cf120e8a354696bd350b' 
-dict = {'token': TOKEN, 'github': 'https://github.com/bdanielsimmons/BDSimmonsCode2040'}
+TOKEN = '6ecdff3c2d49cf120e8a354696bd350b' #Step 1 (Establish ID)
+dict = {'token': TOKEN, 'github': 'https://github.com/bdanielsimmons/BDSimmonsCode2040'} #Fill in dictionary and post to site
 r = requests.post("http://challenge.code2040.org/api/register", json = dict)
 
-dict = {'token': TOKEN}
+
+dict = {'token': TOKEN} #Step 2 (Reset the token)
 string = requests.post("http://challenge.code2040.org/api/reverse", dict)
-reverseString = string.text[::-1]
+reverseString = string.text[::-1] #Slices through the strin backwards and posts to API
 myKey = {'token': TOKEN, 'string': reverseString}
 string = requests.post("http://challenge.code2040.org/api/reverse/validate", myKey)
 
-r = requests.post("http://challenge.code2040.org/api/haystack", data={'token': TOKEN})
-json = r.json()
-needle = json['needle']
-haystack = json['haystack']
-index = haystack.index(needle)
+
+r = requests.post("http://challenge.code2040.org/api/haystack", data={'token': TOKEN}) #Step 3 (Grab data from the API)
+json = r.json() #Convert info from JSON dict, create variable/array
+needle = json['needle'] #Extract array infor
+haystack = json['haystack'] 
+index = haystack.index(needle) #Add index to dict and send back
 r = requests.post("http://challenge.code2040.org/api/haystack/validate", data={'token': TOKEN, 'needle': index})
 
-r = requests.post("http://challenge.code2040.org/api/prefix", data={'token': TOKEN})
+
+URL = "http://challenge.code2040.org/api/prefix"
+r = requests.post(URL, data={'token': TOKEN}) #Step 4 (Get info from the API)
 json = r.json()
-prefix = json['prefix']
+prefix = json['prefix'] #For whatever item we're looking at, if it doesn't have the decoded prefix, add the item to newArray
 array = json['array']
 newArray = []
 for item in array:
     if not item.startswith(prefix):
         newArray.append(item)
-r = requests.post("http://challenge.code2040.org/api/prefix/validate", json={'token': TOKEN, 'array': newArray})
+r = requests.post(URL, json={'token': TOKEN, 'array': newArray}) #Post back to the API
 
-r = requests.post("http://challenge.code2040.org/api/dating", data={'token': TOKEN})
+
+URL = "http://challenge.code2040.org/api/dating/validate"
+r = requests.post(URL, data={'token': TOKEN}) #Step 5 (Gather data from API)
 json = r.json()
 datestamp = json['datestamp']
-interval = json['interval']
+interval = json['interval'] #Take time and time change from the dating API
 current_datestamp = datetime.datetime.strptime(datestamp, "%Y-%m-%dT%H:%M:%SZ")
-new_datestamp = current_datestamp + datetime.timedelta(seconds=int(interval))
+new_datestamp = current_datestamp + datetime.timedelta(seconds=int(interval)) #Scale up the current date by the interval
 new_datestamp_formatted = new_datestamp.strftime("%Y-%m-%dT%H:%M:%SZ")
-r = requests.post("http://challenge.code2040.org/api/dating/validate", data={'token': TOKEN, 'datestamp': new_datestamp_formatted})
+r = requests.post(URL, data={'token': TOKEN, 'datestamp': new_datestamp_formatted})#Post back to the API
